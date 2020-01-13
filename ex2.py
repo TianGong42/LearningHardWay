b40ac9e75ad2eaf1c4d789013813f5f386f6d526
chizhipeng <375032565@qq.com>
2020-01-13 23:40:11 +0800

    提交前三章

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# 一条评论，这是为了让你以后可以阅读你的程序
+# Anything after the # is ignored by python
+print("I could have code like this.") # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out code: 您还可以使用注释来“禁用”或注释掉代码：
+
+# print("This won't run.")
+
+print("This will run.")
---
+++ ex3.py
@@ -0,0 +1,22 @@
+print("I will now count my chickens(我现在要数我的鸡): ")
+
+print("Hens(母鸡)", 25 + 30 / 6)
+print("Roosters（公鸡)", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs（现在我来数鸡蛋):")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that  3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("On, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 >= -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
